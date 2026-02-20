7194376fe7bc1aa144055992fe554f88bc73033d
John Lloyd <johnlloydtubachups@gmail.com>
2026-02-20 12:26:35 +0800

    mobile responsive for homepage

--- resources/js/pages/Shop/Index.vue
+++ resources/js/pages/Shop/Index.vue
@@ -38,16 +38,16 @@ const searchQuery = ref<string>("");
 
     <div class="container mx-auto p-6 w-full flex flex-col justify-center">
         <div class="flex flex-row justify-between items-baseline mb-2">
-            <div class="text-3xl font-bold underline">Components</div>
-            <Link href="/components" class="text-md">See more...</Link>
+            <p class="text-xl md:text-3xl font-bold underline">Components</p>
+            <Link href="/components" class="text-sm md:text-lg">See more...</Link>
         </div>
 
-        <ComponentsCarousel class="basis-1/6 "/>
+        <ComponentsCarousel />
     </div>
 
     <div class="container mx-auto p-6 w-full flex justify-center flex-col items-center">
-        <div class="text-3xl font-bold underline">REVIEWS</div>
-        <div class="text-md mb-4">From our satisfied customers...</div>
+        <div class="text-2xl md:text-3xl font-bold underline">Reviews</div>
+        <div class="text-sm sm:text-lg mb-4">From our satisfied customers...</div>
         <ReviewsCarousel class="basis-1/4"/>
     </div>
 
