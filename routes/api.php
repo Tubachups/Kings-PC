9d1a8caada0dfa899b84c65f4a9ef49387043330
unknown <dandelvo12345@gmail.com>
2026-03-09 12:07:54 +0800

    Cron Job Api

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/cron', function () {
+    Artisan::call('queue:restart');
+    return response()->json(['message' => 'Restart signal sent to all workers.']);
+});
