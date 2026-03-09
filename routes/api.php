7ce723d9f6c6e67d0156a0805d1a8dc76fea7fa8
unknown <dandelvo12345@gmail.com>
2026-03-09 12:20:01 +0800

    POST

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/cron', function () {
-    Artisan::call('queue:work', ['--stop-when-empty' => true]);
+
+Route::post('/cron', function (Request $request) {
+    abort_unless(
+        hash_equals(config('app.cron_token'), (string) $request->header('X-CRON-TOKEN')),
+        403
+    );
+
+    Artisan::call('queue:work', [
+        '--once' => true,
+        '--queue' => 'default',
+        '--tries' => 3,
+    ]);
+
+    return response()->json(['ok' => true]);
 });
